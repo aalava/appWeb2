90a6061ec334f6849fc7291173f9b2804dabfc04
Manuel Alejandro <manalava@gmail.com>
2022-11-14 07:42:51 -0500

    14-11

---
+++ src/learning-react/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import CreateButton from './CreateButton';
+
+function App() {
+  return (
+    <div className="App">
+      <CreateButton text="Enviar"/>
+      <CreateButton text="Resetear"/>
+    </div>
+  );
+}
+
+export default App;
